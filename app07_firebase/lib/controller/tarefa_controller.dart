2e2983599f41687bec2e62026a32236c57f3784d
Rodrigo Plotze <rodrigoplotze@gmail.com>
2024-05-16 20:17:33 -0300

    tarefa controller

--- app07_firebase/lib/controller/tarefa_controller.dart
+++ app07_firebase/lib/controller/tarefa_controller.dart
@@ -3,18 +3,41 @@ import 'package:flutter/material.dart';
 
 import '../model/tarefa.dart';
 import '../view/util.dart';
+import 'login_controller.dart';
+
 
 class TarefaController {
-
-  //Adicionar uma tarefa no Firestore
   void adicionar(context, Tarefa t) {
-
     FirebaseFirestore.instance
         .collection('tarefas')
         .add(t.toJson())
         .then((resultado) => sucesso(context, 'Tarefa adicionada com sucesso'))
         .catchError((e) => erro(context, 'Não foi possível adicionar a tarefa'))
         .whenComplete(() => Navigator.pop(context));
-
   }
+
+  //Listar todas as tarefas do Usuário autenticado
+  listar(){
+    return FirebaseFirestore.instance.collection('tarefas')
+      .where('uid', isEqualTo: LoginController().idUsuario());
+  }
+
+  void atualizar(context,id,Tarefa t){
+    FirebaseFirestore.instance.collection('tarefas')
+      .doc(id)
+      .update(t.toJson())
+      .then((resultado) => sucesso(context, 'Tarefa atualizada com sucesso'))
+      .catchError((e) => erro(context, 'Não foi possível atualizar a tarefa'))
+      .whenComplete(() => Navigator.pop(context));
+  }
+
+  void excluir(context,id){
+    FirebaseFirestore.instance.collection('tarefas')
+      .doc(id)
+      .delete()
+      .then((resultado) => sucesso(context, 'Tarefa excluída com sucesso'))
+      .catchError((e) => erro(context, 'Não foi possível excluir a tarefa'));
+  }
+
+
 }
--- app07_firebase/lib/view/principal_view.dart
+++ app07_firebase/lib/view/principal_view.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers
 
-import 'package:app07_firebase/controller/tarefa_controller.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../controller/login_controller.dart';
+import '../controller/tarefa_controller.dart';
 import '../model/tarefa.dart';
 
 class PrincipalView extends StatefulWidget {
@@ -27,10 +27,8 @@ class _PrincipalViewState extends State<PrincipalView> {
         actions: [
           IconButton(
             onPressed: () {
-
               LoginController().logout();
               Navigator.pop(context);
-              
             },
             icon: Icon(Icons.exit_to_app),
           )
@@ -40,7 +38,90 @@ class _PrincipalViewState extends State<PrincipalView> {
       // BODY
       body: Padding(
         padding: const EdgeInsets.all(20.0),
+        //
+        // LISTAR as tarefas
+        //
+        child: StreamBuilder<QuerySnapshot>(
+          //fluxo de dados
+          stream: TarefaController().listar().snapshots(),
+          //exibição dos dados
+          builder: (context, snapshot) {
+            //verificar a conectividade
+            switch (snapshot.connectionState) {
+              //sem conexão
+              case ConnectionState.none:
+                return Center(
+                  child: Text("Falha na conexão."),
+                );
+
+              //conexão lenta
+              case ConnectionState.waiting:
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+
+              //dados recuperados com sucesso
+              default:
+                final dados = snapshot.requireData;
+                if (dados.size > 0) {
+                  //exibir a lista de tarefas
+                  return ListView.builder(
+                    itemCount: dados.size,
+                    itemBuilder: (context, index) {
+
+                      //ID do documento
+                      String id = dados.docs[index].id;
+
+                      //DADOS armazenados no documento
+                      dynamic item = dados.docs[index].data();
+
+                      return Card(
+                        child: ListTile(
+                          title: Text(item['titulo']),
+                          subtitle: Text(item['descricao']),
+                          //
+                          // Atualizar e Excluir Tarefas
+                          //
+                          trailing: SizedBox(
+                            width: 80,
+                            child: Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {
+
+                                    txtTitulo.text = item['titulo'];
+                                    txtDescricao.text = item['descricao'];
+                                    salvarTarefa(context,docId: id);
+
+                                  },
+                                  icon: Icon(Icons.edit_rounded),
+                                ),
+                                IconButton(
+                                  onPressed: () {
+
+                                    TarefaController().excluir(context, id);
+
+                                  },
+                                  icon: Icon(Icons.delete_rounded),
+                                ),
+                              ],
+                            ),
+                          ),
+                          onTap: (){},
+                        ),
+                      );
+                    },
+                  );
+                } else {
+                  return Center(
+                    child: Text("Nenhuma tarefa encontrada."),
+                  );
+                }
+            }
+          },
+        ),
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           salvarTarefa(context);
@@ -59,7 +140,7 @@ class _PrincipalViewState extends State<PrincipalView> {
       builder: (BuildContext context) {
         // retorna um objeto do tipo Dialog
         return AlertDialog(
-          title: Text((docId==null )? "Adicionar Tarefa": "Editar Tarefa"),
+          title: Text((docId == null) ? "Adicionar Tarefa" : "Editar Tarefa"),
           content: SizedBox(
             height: 250,
             width: 300,
@@ -99,7 +180,6 @@ class _PrincipalViewState extends State<PrincipalView> {
             ElevatedButton(
               child: Text("salvar"),
               onPressed: () {
-
                 //criação do objeto
                 var t = Tarefa(
                   LoginController().idUsuario(),
@@ -107,10 +187,14 @@ class _PrincipalViewState extends State<PrincipalView> {
                   txtDescricao.text,
                 );
 
-                TarefaController().adicionar(context, t);
+                if (docId == null){
+                  TarefaController().adicionar(context, t);
+                }else{
+                  TarefaController().atualizar(context, docId, t);
+                }
+
                 txtTitulo.clear();
                 txtDescricao.clear();
-
               },
             ),
           ],
